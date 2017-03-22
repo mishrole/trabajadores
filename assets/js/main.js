951a26e34f2ec24ad2d24e9d507af01683e40a8c
Mitchell Rodríguez <mitrodle@gmail.com>
2017-03-22 13:24:36 -0500

    re-indentando

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,22 @@
 var trabajadores = [{ nombre : "Arabela", apellido : "Rojas", rol : "T.A", cumpleanios : "23 de enero" },
-{ nombre : "Michelle", apellido : "Seguil", rol : "T.A", cumpleanios : "25 de noviembre" },
-{ nombre : "Meche", apellido : "Zubieta", rol : "T.A", cumpleanios : "02 de marzo" },
-{ nombre : "Papu", apellido : "Rivariola", rol : "Psicóloga", cumpleanios : "10 de octubre" },
-{ nombre : "Gian", apellido : "Corzo", rol : "Profesor", cumpleanios : "23 de enero" } ]
+					{ nombre : "Michelle", apellido : "Seguil", rol : "T.A", cumpleanios : "25 de noviembre" },
+					{ nombre : "Meche", apellido : "Zubieta", rol : "T.A", cumpleanios : "02 de marzo" },
+					{ nombre : "Papu", apellido : "Rivariola", rol : "Psicóloga", cumpleanios : "10 de octubre" },
+					{ nombre : "Gian", apellido : "Corzo", rol : "Profesor", cumpleanios : "23 de enero" } ];
 
 var info = document.getElementById("info");
 
 info.innerHTML= "";
 
-  trabajadores.forEach(function(element){
-  info.innerHTML += "<div><ul><li>" + "Nombre: " + element.nombre + "</li>"+
-                              "<li>" + "Apellido: " + element.apellido + "</li>"+
-                              "<li>" + "Rol: " + element.rol + "</li>"+
-                              "<li>" + "Cumpleaños: " + element.cumpleanios + "</li></ul></div>";
-})
+trabajadores.forEach(function(element){
+
+	info.innerHTML += "<div>
+  						<ul>
+  							<li>" + "Nombre: " + element.nombre + "</li>"+
+                           "<li>" + "Apellido: " + element.apellido + "</li>"+
+                           "<li>" + "Rol: " + element.rol + "</li>"+
+                           "<li>" + "Cumpleaños: " + element.cumpleanios + "</li>
+                        </ul>
+                   	 </div>";
+                   	 
+});
